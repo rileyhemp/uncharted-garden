ca19244538e690c3b90250397db03fec0f4e9932
Riley Hemphill <rileyhemp@gmail.com>
2020-01-26 16:33:33 -0500

    Infinite slider functioning

--- src/InfiniteSlider.js
+++ src/InfiniteSlider.js
@@ -1,13 +1,28 @@
 import React from 'react'
 import styled from 'styled-components'
 
+const activeState = `
+	transform: translateX(0%);
+	z-index: 2;
+	opacity: 1;
+`
+
+const nextState = `
+transform: translateX(100%);
+`
+
+const previousState = `
+transform: translateX(-100%);
+`
+
 const Arrow = styled.button`
 	z-index: 100;
 `
 const Wrapper = styled.div`
 	display: flex;
 	justify-content: space-between;
-	width: 250px;
+	width: 150px;
+	overflow: hidden;
 	position: relative;
 	`
 const Slide = styled.div`
@@ -15,6 +30,13 @@ const Slide = styled.div`
 		width: 100%;
 		text-align: center;
 		position: absolute;
+		transition: all 0.3s;
+		opacity: 0;
+		${props => {
+		return props.position === 'active' ? activeState :
+			props.position === 'next' ? nextState :
+				props.position === 'previous' ? previousState : null
+	}}
 	`
 
 export default class InfiniteSlider extends React.Component {
@@ -30,16 +52,19 @@ export default class InfiniteSlider extends React.Component {
 		}))
 	}
 	getSlidePosition(slideKey) {
-		if (slideKey == this.state.index) return 'active'
-		else if (slideKey < this.state.index) return 'previous'
-		else if (slideKey > this.state.index) return 'next'
+		let index = this.state.index
+		if (slideKey == index) return 'active'
+		else if (slideKey === this.state.slides.length - 1 && index == 0) return 'previous'
+		else if (slideKey === 0 && index == this.state.slides.length - 1) return 'next'
+		else if (slideKey < index) return 'previous'
+		else if (slideKey > index) return 'next'
 	}
 	render() {
 		return (
 			<Wrapper>
 				<Arrow onClick={() => this.changeIndex(-1)}>-</Arrow>
 				{this.state.slides ? this.state.slides.map(slide => {
-					return <Slide key={slide.key} position={this.getSlidePosition(slide.key)}>{slide}</Slide>
+					return <Slide key={slide.key} position={this.getSlidePosition(parseInt(slide.key))}>{slide}</Slide>
 				}) : null}
 				<Arrow onClick={() => this.changeIndex(1)}>+</Arrow>
 			</Wrapper>
