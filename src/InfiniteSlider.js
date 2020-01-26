4fbb716aa88c87ecb58536d10e1fc9526cba8460
Riley Hemphill <rileyhemp@gmail.com>
2020-01-26 12:23:34 -0500

    differentiated starting vs updated slides

--- src/InfiniteSlider.js
+++ src/InfiniteSlider.js
@@ -2,17 +2,19 @@ import React, { useState, useEffect } from 'react'
 import styled from 'styled-components'
 
 export default function InfiniteSlider(props) {
-	console.log(props)
+	// console.log(props)
 	const Arrow = styled.button``
 	const Wrapper = styled.div``
 	const Slide = styled.div``
 	const [index, setIndex] = useState(props.startingIndex || 0)
 	const [slides, changeSlide] = useState(props.children)
 	useEffect(() => {
-
+		if (index == props.startingIndex) {
+			console.log('starting slides', slides)
+		} else console.log('updated slides', slides)
 	})
-	console.log('index:', index)
-	console.log('slides:', slides)
+	// console.log('index:', index)
+	// console.log('slides:', slides)
 	return (
 		<React.Fragment>
 			<Arrow onClick={() => setIndex(index - 1)}>-</Arrow>
